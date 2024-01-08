b356c69a37c4050f4ca5e14f96b53ff1b4c9a5ae
Linet-Mukai1 <linetmukai9@gmail.com>
2024-01-08 15:18:53 +0300

    initial commit

---
+++ main_app/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+def home(request):
+    return render(request, 'index.html')
+
+
+def shop(request):
+    return render(request, 'product.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
